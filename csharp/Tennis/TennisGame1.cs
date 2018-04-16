a749c72b8d3de155be37106ede0639bdd0bef5f6
rosiehighsmith <rosie.highsmith@gmail.com>
2018-04-16 07:50:53 -0700

    first pass of refactoring tennis1.cs

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -2,8 +2,8 @@ namespace Tennis
 {
     class TennisGame1 : ITennisGame
     {
-        private int m_score1 = 0;
-        private int m_score2 = 0;
+        private int player1Score = 0;
+        private int player2Score = 0;
         private string player1Name;
         private string player2Name;
 
@@ -16,65 +16,84 @@ namespace Tennis
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                m_score1 += 1;
+                player1Score += 1;
             else
-                m_score2 += 1;
+                player2Score += 1;
         }
 
         public string GetScore()
         {
             string score = "";
-            var tempScore = 0;
-            if (m_score1 == m_score2)
+            if (player1Score == player2Score)
             {
-                switch (m_score1)
-                {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-
-                }
+                score = AssignTiedScoreValues();
             }
-            else if (m_score1 >= 4 || m_score2 >= 4)
+            else if (player1Score >= 4 || player2Score >= 4)
             {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                score = AssignAdvantageOrWin();
             }
             else
             {
-                for (var i = 1; i < 3; i++)
+                AssignUntiedScore(ref score);
+            }
+            return score;
+        }
+
+        private void AssignUntiedScore(ref string score)
+        {
+            for (var i = 1; i < 3; i++)
+            {
+                var tempScore = 0;
+                if (i == 1) tempScore = player1Score;
+                else { score += "-"; tempScore = player2Score; }
+                switch (tempScore)
                 {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
+                    case 0:
+                        score += "Love";
+                        break;
+                    case 1:
+                        score += "Fifteen";
+                        break;
+                    case 2:
+                        score += "Thirty";
+                        break;
+                    case 3:
+                        score += "Forty";
+                        break;
                 }
             }
+        }
+
+        private string AssignAdvantageOrWin()
+        {
+            string score;
+            var minusResult = player1Score - player2Score;
+            if (minusResult == 1) score = $"Advantage {player1Name}";
+            else if (minusResult == -1) score = $"Advantage {player2Name}";
+            else if (minusResult >= 2) score = $"Win for {player1Name}";
+            else score = $"Win for {player2Name}";
+            return score;
+        }
+
+        private string AssignTiedScoreValues()
+        {
+            string score;
+            switch (player1Score)
+            {
+                case 0:
+                    score = "Love-All";
+                    break;
+                case 1:
+                    score = "Fifteen-All";
+                    break;
+                case 2:
+                    score = "Thirty-All";
+                    break;
+                default:
+                    score = "Deuce";
+                    break;
+            }
+
             return score;
         }
     }
